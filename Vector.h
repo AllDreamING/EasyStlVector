d1b7d9c64ff606e2c204601507c12606cbe41b28
wuchen <1031083335@qq.com>
2023-05-06 03:57:14 -0700

    some

--- Vector.h
+++ Vector.h
@@ -10,6 +10,20 @@ namespace selfstl
         void show();    //展示VECTOR内部情况
 
         void push_back(const T& val);   //插入
+        void pop_back();//移除尾部元素
+        int size() const;//数组大小
+        int capacity() const;//数组容量
+        void reserve (int n);//数组容量是否大于n,否则扩容
+        void resize(int size); //指定数组大小为size
+        T& at(int idx);//返回idx处元素的引用
+        T& operator [](int idx);//重载[]
+        bool empty() const;//数组是否为空
+        void clear();//清空数组
+        T& front() const;//返回数组头的引用
+        T& back() const;//返回数组尾部的引用
+        void swap(Vector<T>& other);//交换两个容器
+        T* data();//直接返回数组首地址（头指针）
+        Vector<T>& operator = (const Vector<T>& other);//重载=
 
 
     private:
@@ -75,6 +89,182 @@ void Vector<T>::push_back(const T& val){
     return ;
 }
 
+template <typename T>
+void Vector<T>::pop_back(){
+    if(m_size > 0){
+        m_size -= 1;
+    }
+    return ;
+}
 
-    
+template <typename T>
+int Vector<T>::size() const
+{
+    return m_size;
+}
+
+template <typename T>
+int Vector<T>::capacity() const
+{
+    return m_capacity;
+}
+
+template <typename T>
+void Vector<T>::reserve(int n)
+{
+    while(m_capacity < n){
+        if(m_capacity == 0){
+            m_capacity = 1;
+        }else{
+            m_capacity *= 2;
+        }
+    }
+    T* data = new T[m_capacity];
+    for(int i=0;i<m_size;i++){
+        data[i] = m_data[i];
+    }
+    //删除旧数组
+    if(m_data!=nullptr){
+        delete [] m_data;
+        m_data = nullptr;
+    }
+    m_data = data;
+    return ;
+}
+
+template <typename T>
+void Vector<T>::resize(int size)
+{
+    if(m_size >= size){
+        m_size = size;
+        return ;
+    }
+    if(m_capacity >= size){
+        for(int i=m_size;i<size;i++){
+            m_data[i] = T();
+        }
+        m_size = size;
+        return ;
+    }
+    while(m_capacity < size){
+        if(m_capacity == 0){
+            m_capacity = 1;
+        }else{
+            m_capacity *= 2;
+        }
+    }
+    T* data = new T[m_capacity];
+    for(int i=0;i<m_size;i++){
+        data[i] = m_data[i];
+    }
+    for(int i=m_size;i<size;i++){
+        data[i] = T();
+    }
+    //删除旧数组
+    if(m_data!=nullptr){
+        delete [] m_data;
+        m_data = nullptr;
+    }
+    m_data = data;
+    m_size = size;
+    return ;
+
+}
+
+template <typename T>
+T &Vector<T>::at(int idx)
+{
+    if(idx < 0 || idx > m_size){
+        throw std::logic_error("out of range!");
+    }
+    return m_data[idx];
+}
+
+template <typename T>
+T &Vector<T>::operator[](int idx)
+{
+    return at(idx);
+}
+
+template <typename T>
+bool Vector<T>::empty() const
+{
+    return m_size == 0;
+}
+
+template <typename T>
+void Vector<T>::clear()
+{
+    m_size = 0;
+    return ;
+}
+
+template <typename T>
+T &Vector<T>::front() const
+{
+    if(m_size < 0){
+        throw std::logic_error("vector is empty!");
+    }
+    return m_data[0];
+}
+
+template <typename T>
+T &Vector<T>::back() const
+{
+    if(m_size < 0){
+        throw std::logic_error("vector is empty!");
+    }
+    return m_data[m_size-1];
+}
+
+template <typename T>
+void Vector<T>::swap(Vector<T> &other)
+{
+    T* data = other.m_data;
+    int size = other.m_size;
+    int capacity = other.m_capacity;
+
+    other.m_data = m_data;
+    other.m_size = m_size;
+    other.m_capacity = m_capacity;
+
+    m_data = data;
+    m_size = size;
+    m_capacity = capacity;
+
+    return ;
+}
+
+template <typename T>
+T *Vector<T>::data()
+{
+    return m_data;
+}
+
+template <typename T>
+Vector<T> &Vector<T>::operator=(const Vector<T> &other)
+{
+    if(m_capacity < other.m_size){
+        if(m_data!=nullptr){
+            delete [] m_data;
+            m_data = nullptr;
+            m_size = 0;
+            m_capacity = 0;
+        }
+        while(m_capacity < other.m_size){
+            if(m_capacity == 0){
+                m_capacity = 1;
+            }else{
+                m_capacity *= 2;
+            }
+        }
+    }
+    m_data = new T[m_capacity];
+    for(int i=0;i<other.m_size;i++){
+        m_data[i] = other.m_data[i];
+    }
+    m_size = other.m_size;
+    return *this;
+}
+
 } // namespace selfstl
--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 using namespace selfstl;
 
 int main(){
-
+    //creat
     Vector<int> v;
+    //push_back
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
@@ -13,6 +14,60 @@ int main(){
 
     
 
+    // //=
+    // Vector<int> v2;
+    // v2 = v;
+    // v2.show();
+
+    // //data
+    // int *p = v.data();
+    // *p = 10;
+    // v.show();
+
+    // //swap
+    // Vector<int> v2;
+    // v2.push_back(4);
+    // v2.push_back(5);
+    // v2.show();
+    // v2.swap(v);
+    // v2.show();
+
+    // //front
+    // std::cout<<"v.front() :"<<v.front()<<std::endl;
+
+    // //back
+    // std::cout<<"v.back() :"<<v.back()<<std::endl;
+
+    // //pop_back
+    // v.pop_back();
+    // v.show();
+
+    // //size
+    // std::cout<<"m_size :"<<v.size()<<std::endl;
+
+    // //capacity
+    // std::cout<<"m_capacity :"<<v.capacity()<<std::endl;
+
+    // //reverse
+    // v.reserve(16);
+    // v.show();
+
+    // //resize
+    // v.resize(32);
+    // v.show();
+
+    // //at
+    // std::cout<<"v[2] :"<<v.at(2)<<std::endl;
+
+    // //[]
+    // std::cout<<"v[2] :"<<v[2]<<std::endl;
+
+    // //empty
+    // std::cout<<"v is empty :"<<v.empty()<<std::endl;
+
+    // //clear
+    // v.clear();
+    // v.show();
 
     std::cout<<"main cout test"<<std::endl;
     return 0;
